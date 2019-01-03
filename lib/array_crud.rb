1811fd4fe8a258c19402a9cffafff2bc31674cef
ipsita Pattanaik <ipsitapattanaik.cse@gmail.com>
2019-01-03 23:12:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_array = ["bulldog", "terrier", "poodle","hound"]
+  ["bulldog", "terrier", "poodle","hound"]
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array << "arrays!"
+  array << 
 end
 
 def add_element_to_start_of_array(array, element)
